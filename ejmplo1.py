2ac41d4c7ad4a0320cd5d012241f6465e0c96472
LYBE1978 <lisbethybe12345@gmail.com>
2025-10-14 19:00:36 -0700

    comentario

---
+++ ejmplo1.py
@@ -0,0 +1,24 @@
+
+print("Ejemplo de funciones")
+
+
+def area_rectangulo(l1, l2):
+    #
+    area = l1 * l2
+    return area
+
+print(area_rectangulo(20, 30))
+
+def area_triangulo(b, h):
+    ''' calcula el area del triangulo'''
+    area = (b * h)/2
+    return area 
+
+    #print("area del triangulo: ", area)
+print(area_triangulo(10,10))
+
+a = 20
+def suma(b, c):
+    suma = a + b + c
+    return suma
+print(suma(2, 3))
